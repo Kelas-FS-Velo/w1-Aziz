a6969a4f5876dd07dc601eb67ed2c5cf7ee67589
AzizLike29 <abdulfirdaus590@gmail.com>
2025-03-15 14:53:06 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl mx-auto mt-20">
+      <picture class="rounded-lg block overflow-hidden">
+        <img class="hover:scale-125" src="https://picsum.photos/360/240" />
+      </picture>
+
+      <h1 class="text-center mt-4 mb-2 text-xl font-bold">
+        Abdul Aziz Firdaus
+      </h1>
+      <p class="text-justify text-sm text-gray-600">
+        Saya merupakan seorang web developer yang berfokus pada pengembangan
+        aplikasi berbasis web. Saya memiliki pengalaman dalam pengembangan
+        aplikasi berbasis web menggunakan teknologi modern seperti Vue.js,
+        Nuxt.js, dan Tailwind CSS.
+      </p>
+      <ul class="flex justify-evenly flex-row list-none mt-3 mb-0">
+        <li
+          class="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-lg"
+        >
+          <a href="https://www.linkedin.com/in/abdulazizfirdaus">LinkedIn</a>
+        </li>
+        <li
+          class="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-lg"
+        >
+          <a href="https://www.instagram.com/abdulaaziz011">Instagram</a>
+        </li>
+        <li
+          class="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-lg"
+        >
+          <a href="https://github.com/AzizLike29">Github</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
